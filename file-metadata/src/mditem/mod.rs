a50fc0587919ac0cb5d9363f513cef339415bd08
Domenico Shura <shura1991@gmail.com>
2019-06-16 05:01:40 +0200

    Add MDItem::from_string and MDItem::from_url

--- file-metadata/src/mditem/mod.rs
+++ file-metadata/src/mditem/mod.rs
@@ -2,7 +2,9 @@ extern crate file_metadata_mditem_macros;
 
 use std::marker::PhantomData;
 
+use core_foundation_sys::base::{CFAllocatorRef, kCFAllocatorDefault};
 use core_foundation_sys::base::{TCFTypeRef, CFTypeID};
+use core_foundation::url::{CFURL, CFURLRef};
 use core_foundation::string::{CFString, CFStringRef};
 use core_foundation::array::{CFArray, CFArrayRef};
 use core_foundation::base::TCFType;
@@ -14,6 +16,8 @@ pub type MDItemRef = *const __MDItemRef;
 
 #[link(name = "CoreServices", kind = "framework")]
 extern "C" {
+    fn MDItemCreate(allocator: CFAllocatorRef, path: CFStringRef) -> MDItemRef;
+    fn MDItemCreateWithURL(allocator: CFAllocatorRef, url: CFURLRef) -> MDItemRef;
     fn MDItemCopyAttribute(item: MDItemRef, name: CFStringRef) -> *const c_void;
     fn MDItemCopyAttributeNames(item: MDItemRef) -> CFArrayRef;
     fn MDItemGetTypeID() -> CFTypeID;
@@ -38,6 +42,26 @@ declare_TCFType!{
 impl_TCFType!(MDItem, MDItemRef, MDItemGetTypeID);
 
 impl MDItem {
+    pub fn from_string(path: CFString) -> Option<MDItem> {
+        let ptr = unsafe { MDItemCreate(kCFAllocatorDefault, path.as_concrete_TypeRef()) };
+
+        if ptr.is_null() {
+            None
+        } else {
+            Some(unsafe { TCFType::wrap_under_create_rule(ptr) })
+        }
+    }
+
+    pub fn from_url(url: CFURL) -> Option<MDItem> {
+        let ptr = unsafe { MDItemCreateWithURL(kCFAllocatorDefault, url.as_concrete_TypeRef()) };
+
+        if ptr.is_null() {
+            None
+        } else {
+            Some(unsafe { TCFType::wrap_under_create_rule(ptr) })
+        }
+    }
+
     pub fn attributes(&self) -> CFArray<CFString> {
         unsafe {
             TCFType::wrap_under_create_rule(MDItemCopyAttributeNames(self.0))
@@ -58,12 +82,16 @@ impl MDItem {
 
 #[cfg(test)]
 mod tests {
-    use super::attributes::URL;
+    use super::{MDItem, attributes::{CFBundleIdentifier, DisplayName}};
+    use core_foundation::url::CFURL;
     use core_foundation::string::CFString;
-    use core_foundation::base::TCFType;
+    use std::path::Path;
 
     #[test]
-    fn it_works() {
-        println!("{:#?}", unsafe { CFString::wrap_under_get_rule(URL.key()) });
+    fn get_safari_bundle_id_and_display_name() {
+        let safari_url = CFURL::from_path(Path::new("/Applications/Safari.app"), true).unwrap();
+        let safari_item = MDItem::from_url(safari_url).unwrap();
+        assert_eq!(safari_item.get(CFBundleIdentifier), Some(CFString::new("com.apple.Safari")));
+        assert_eq!(safari_item.get(DisplayName), Some(CFString::new("Safari")));
     }
 }
